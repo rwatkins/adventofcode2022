cb47f96a321b02faeaf567d30659fc6d9165f71d
Riley Watkins <riley@rileywatkins.com>
2022-12-02 15:23:33 -0800

    Add Day 2 part 2

--- day2/src/day2/core.clj
+++ day2/src/day2/core.clj
@@ -1,31 +1,20 @@
 (ns day2.core
-  (:require [clojure.spec.alpha :as s]
-            [clojure.core.match :refer [match]]
-            [clojure.string :refer [split split-lines]]))
+  (:require
+   [clojure.core.match :refer [match]]
+   [clojure.string :refer [split split-lines]]))
 
-(s/check-asserts true)
-(s/def :day2/shapes #{:rock :paper :scissors})
+(defn code-to-keyword-part1 [code]
+  (get {"A" :rock "B" :paper "C" :scissors "X" :rock "Y" :paper "Z" :scissors} code))
 
-(defn code-to-keyword [code]
-  {:post [(s/assert :day2/shapes %)]}
-  (get {"A" :rock
-        "B" :paper
-        "C" :scissors
-        "X" :rock
-        "Y" :paper
-        "Z" :scissors}
-       code))
+(defn code-to-keyword-part2 [code]
+  (get {"A" :rock "B" :paper "C" :scissors "X" :lose "Y" :draw "Z" :win} code))
 
 (defn shape-score [shape]
-  {:pre [(s/assert :day2/shapes shape)]}
   (get {:rock 1 :paper 2 :scissors 3} shape))
 
 (defn round-score
   "0 if you lost, 3 if the round was a draw, and 6 if you won"
   [opponent me]
-  {:pre [(s/assert :day2/shapes opponent)
-         (s/assert :day2/shapes me)]
-   :post [(s/assert number? %)]}
   (if (= opponent me) 3
       (match [opponent me]
         [:rock :paper] 6
@@ -39,19 +28,44 @@
   (+ (shape-score me)
      (round-score opponent me)))
 
-(defn line-to-keywords [s]
-  (mapv code-to-keyword (split s #" ")))
-
-(defn sum-scores
-  "Take the input as a single string and compute the sum of the scores 
+(defn sum-scores-part1
+  "Take the input as a single string and compute the sum of the scores
    from each round"
   [input]
   (let [lines (split-lines input)
-        rounds (map line-to-keywords lines)
+        rounds (map #(map code-to-keyword-part1 (split % #" ")) lines)
+        scores (map #(apply score %) rounds)
+        result (reduce + scores)]
+    result))
+
+(defn shape-for-outcome
+  "Given the opponent's shape and the outcome we need, returns the shape that
+   will produce the outcome"
+  [opponent outcome]
+  (if (= outcome :draw)
+    opponent
+    (match [opponent outcome]
+      [:rock :win] :paper
+      [:rock :lose] :scissors
+      [:paper :win] :scissors
+      [:paper :lose] :rock
+      [:scissors :win] :rock
+      [:scissors :lose] :paper)))
+
+(defn sum-scores-part2
+  "Take the input as a single string, find the shapes to match the expected
+   outcomes, and compute the sum of the scores from each round"
+  [input]
+  (let [lines (split-lines input)
+        outcomes (map #(map code-to-keyword-part2 (split % #" ")) lines)
+        rounds (map #(let [shape (apply shape-for-outcome %)]
+                       [(first %) shape])
+                    outcomes)
         scores (map #(apply score %) rounds)
         result (reduce + scores)]
     result))
 
 (defn -main []
   (let [input (slurp "day2_input.txt")]
-    (println "Part 1:" (sum-scores input))))
+    (println "Part 1:" (sum-scores-part1 input))
+    (println "Part 2:" (sum-scores-part2 input))))
--- day2/test/day2/core_test.clj
+++ day2/test/day2/core_test.clj
@@ -1,7 +1,11 @@
 (ns day2.core-test
-  (:require [clojure.test :refer :all]
-            [day2.core :refer :all]))
+  (:require [clojure.test :refer [deftest testing is]]
+            [day2.core :refer [sum-scores-part1 sum-scores-part2]]))
 
-(deftest test-sum-scores
-  (testing "sum-scores using example input"
-    (is (= 15 (sum-scores "A Y\nB X\nC Z")))))
+(deftest test-sum-scores-part1
+  (testing "sum-scores-part1 using example input"
+    (is (= 15 (sum-scores-part1 "A Y\nB X\nC Z")))))
+
+(deftest test-sum-scores-part2
+  (testing "sum-scores-part2 using example input"
+    (is (= 12 (sum-scores-part2 "A Y\nB X\nC Z")))))
