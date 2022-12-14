9a91bb8027b00cb08dcfe3ef72a2325fcab04f26
Riley Watkins <riley@rileywatkins.com>
2022-12-14 15:55:44 -0800

    Day 11 cleanup

--- day11/app/Main.hs
+++ day11/app/Main.hs
@@ -22,22 +22,26 @@ type PendingThrows = Map.Map Int [Int]
 
 type InspectCounts = Map.Map Int Int
 
-data Operation
-  = Add String String
-  | Mult String String
-  deriving (Show)
+newtype Operation = Operation (Item -> Item)
+
+unOperation :: Operation -> Item -> Item
+unOperation (Operation op) = op
+
+instance Show Operation where
+  show _ = "<Operation>"
 
 data Monkey = Monkey
   { items :: [Item],
     operation :: Operation,
     prime :: Int,
-    throwTo :: (Int, Int) -- if test i then fst throwTo else snd throwTo
+    throwTo :: (Int, Int), -- if test i then fst throwTo else snd throwTo
+    inspected :: Int
   }
   deriving (Show)
 
 data Throw = Throw {item :: Item, monkey :: Int} deriving (Show)
 
-type World = ([Monkey], PendingThrows, InspectCounts)
+type World = ([Monkey], PendingThrows)
 
 divisibleBy :: Int -> Item -> Bool
 divisibleBy n item = item `mod` n == 0
@@ -56,18 +60,18 @@ divisibleBy n item = item `mod` n == 0
 
 monkeyInspectItem :: Monkey -> Item -> (Item -> Item) -> Throw
 monkeyInspectItem monkey@Monkey {operation, prime, throwTo} item reduceWorry =
-  let it = applyOperation operation item
-      itemDiv3 = reduceWorry it
+  let it = unOperation operation item
+      reducedItem = reduceWorry it
       throwToMonkey =
-        if divisibleBy prime itemDiv3
+        if divisibleBy prime reducedItem
           then fst throwTo
           else snd throwTo
-   in Throw {item = itemDiv3, monkey = throwToMonkey}
+   in Throw {item = reducedItem, monkey = throwToMonkey}
 
 monkeyInspectAll :: Monkey -> (Item -> Item) -> (Monkey, [Throw])
 monkeyInspectAll monkey reduceWorry =
   let throws = L.foldl' f [] monkey.items
-   in (monkey {items = []}, throws)
+   in (monkey {items = [], inspected = monkey.inspected + length throws}, throws)
   where
     f throws item = monkeyInspectItem monkey item reduceWorry : throws
 
@@ -77,67 +81,124 @@ mapSwap key value m = (Map.lookup key m, Map.insert key value m)
 emptyPendingThrows :: PendingThrows
 emptyPendingThrows = Map.fromList [(i, []) | i <- [0 .. 7]]
 
-emptyInspectCounts :: InspectCounts
-emptyInspectCounts = Map.fromList [(i, 0) | i <- [0 .. 7]]
-
 doRound :: World -> (Item -> Item) -> World
-doRound (monkeys, pendingThrows, inspectCounts) reduceWorry =
+doRound (monkeys, pendingThrows) reduceWorry =
   -- Run a single round, returning the updated list of monkeys, a map of
   -- monkeyIndex -> pending throws, and a map of monkeyIndex -> items
   -- inspected.
-  L.foldl' f ([], pendingThrows, inspectCounts) $ zip [0 ..] monkeys
+  foldl f ([], pendingThrows) $ zip [0 ..] monkeys
   where
     f :: World -> (Int, Monkey) -> World
-    f (seenMonkeys, pendingThrows, counts) (monkeyIdx, monkey) =
+    f (seenMonkeys, pendingThrows) (monkeyIdx, monkey) =
       let (mpending, pending) = mapSwap monkeyIdx [] pendingThrows
+          newItems = monkey.items ++ fromMaybe [] mpending
           -- Inspect all items
-          (newMonkey, throws) = monkeyInspectAll monkey {items = monkey.items ++ fromMaybe [] mpending} reduceWorry
+          (newMonkey, throws) = monkeyInspectAll monkey {items = newItems} reduceWorry
           -- Record throws
-          newPending = L.foldl' (\m th -> Map.adjust (++ [th.item]) th.monkey m) pending throws
-          -- Record count of items inspected
-          newInspectCounts = Map.adjust (+ length throws) monkeyIdx counts
-       in (seenMonkeys ++ [newMonkey], newPending, newInspectCounts)
-
-applyOperation :: Operation -> Item -> Item
-applyOperation op item =
-  case op of
-    Add x y -> parse x + parse y
-    Mult x y -> parse x * parse y
-  where
-    parse :: String -> Int
-    parse "old" = item
-    parse s = read s
+          newPending = foldl (\m th -> Map.adjust (++ [th.item]) th.monkey m) pending throws
+       in (seenMonkeys ++ [newMonkey], newPending)
 
 testInput :: [Monkey]
 testInput =
-  [ Monkey {items = [79, 98], operation = Mult "old" "19", prime = 23, throwTo = (2, 3)},
-    Monkey {items = [54, 65, 75, 74], operation = Add "old" "6", prime = 19, throwTo = (2, 0)},
-    Monkey {items = [79, 60, 97], operation = Mult "old" "old", prime = 13, throwTo = (1, 3)},
-    Monkey {items = [74], operation = Add "old" "3", prime = 17, throwTo = (0, 1)}
+  [ Monkey
+      { items = [79, 98],
+        operation = Operation (* 19),
+        prime = 23,
+        throwTo = (2, 3),
+        inspected = 0
+      },
+    Monkey
+      { items = [54, 65, 75, 74],
+        operation = Operation (+ 6),
+        prime = 19,
+        throwTo = (2, 0),
+        inspected = 0
+      },
+    Monkey
+      { items = [79, 60, 97],
+        operation = Operation (\x -> x * x),
+        prime = 13,
+        throwTo = (1, 3),
+        inspected = 0
+      },
+    Monkey
+      { items = [74],
+        operation = Operation (+ 3),
+        prime = 17,
+        throwTo = (0, 1),
+        inspected = 0
+      }
   ]
 
 realInput :: [Monkey]
 realInput =
-  [ Monkey {items = [52, 78, 79, 63, 51, 94], operation = Mult "old" "13", prime = 5, throwTo = (1, 6)},
-    Monkey {items = [77, 94, 70, 83, 53], operation = Add "old" "3", prime = 7, throwTo = (5, 3)},
-    Monkey {items = [98, 50, 76], operation = Mult "old" "old", prime = 13, throwTo = (0, 6)},
-    Monkey {items = [92, 91, 61, 75, 99, 63, 84, 69], operation = Add "old" "5", prime = 11, throwTo = (5, 7)},
-    Monkey {items = [51, 53, 83, 52], operation = Add "old" "7", prime = 3, throwTo = (2, 0)},
-    Monkey {items = [76, 76], operation = Add "old" "4", prime = 2, throwTo = (4, 7)},
-    Monkey {items = [75, 59, 93, 69, 76, 96, 65], operation = Mult "old" "19", prime = 17, throwTo = (1, 3)},
-    Monkey {items = [89], operation = Add "old" "2", prime = 19, throwTo = (2, 4)}
+  [ Monkey
+      { items = [52, 78, 79, 63, 51, 94],
+        operation = Operation (* 13),
+        prime = 5,
+        throwTo = (1, 6),
+        inspected = 0
+      },
+    Monkey
+      { items = [77, 94, 70, 83, 53],
+        operation = Operation (+ 3),
+        prime = 7,
+        throwTo = (5, 3),
+        inspected = 0
+      },
+    Monkey
+      { items = [98, 50, 76],
+        operation = Operation (\x -> x * x),
+        prime = 13,
+        throwTo = (0, 6),
+        inspected = 0
+      },
+    Monkey
+      { items = [92, 91, 61, 75, 99, 63, 84, 69],
+        operation = Operation (+ 5),
+        prime = 11,
+        throwTo = (5, 7),
+        inspected = 0
+      },
+    Monkey
+      { items = [51, 53, 83, 52],
+        operation = Operation (+ 7),
+        prime = 3,
+        throwTo = (2, 0),
+        inspected = 0
+      },
+    Monkey
+      { items = [76, 76],
+        operation = Operation (+ 4),
+        prime = 2,
+        throwTo = (4, 7),
+        inspected = 0
+      },
+    Monkey
+      { items = [75, 59, 93, 69, 76, 96, 65],
+        operation = Operation (* 19),
+        prime = 17,
+        throwTo = (1, 3),
+        inspected = 0
+      },
+    Monkey
+      { items = [89],
+        operation = Operation (+ 2),
+        prime = 19,
+        throwTo = (2, 4),
+        inspected = 0
+      }
   ]
 
 doRounds :: Int -> (Item -> Item) -> World -> World
 doRounds n reduceWorry world
-  | n > 0 = let w = doRound world reduceWorry in doRounds (n - 1) reduceWorry w
+  | n > 0 = doRounds (n - 1) reduceWorry $ doRound world reduceWorry
   | otherwise = world
 
 monkeyBusiness :: Int -> (Item -> Item) -> Int
 monkeyBusiness rounds reduceWorry =
-  let (_, _, inspectCounts) = doRounds rounds reduceWorry (realInput, emptyPendingThrows, emptyInspectCounts)
-      top2 = take 2 . reverse . sort . map snd $ Map.toList inspectCounts
-   in L.foldl' (*) 1 top2
+  let (monkeys, _) = doRounds rounds reduceWorry (realInput, emptyPendingThrows)
+   in product . take 2 . reverse . sort $ map inspected monkeys
 
 part1 :: Int
 part1 = monkeyBusiness 20 (`div` 3)
@@ -149,7 +210,6 @@ part2 = monkeyBusiness 10000 (`mod` lcm')
 
 main :: IO ()
 main = do
-  --   input <- readFile "day11_input.txt"
   putStr "Part 1: "
   print part1
   putStr "Part 2: "
